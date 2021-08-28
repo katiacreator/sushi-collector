454dc5cc74f4ac1a3273179f5567706e9332efb4
katiacreator <katia.creator@gmail.com>
2021-08-28 19:40:45 -0400

    created home and about url and views, created templates directory

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+#define the home view
+def home(request):
+  return HttpResponse('<h1>Hello Sushi Fans!</h1>')
+
+#define the about view
+def about(request):
+  return HttpResponse('<h1>About Sushi</h1>')
+
