34d9125b614388aeffe0285cad01ed5e9f607f57
katiacreator <katia.creator@gmail.com>
2021-08-28 21:36:23 -0400

    completed MVC for all sushi page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('sushi/', views.sushi_index, name='sushi_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Add the Sushi class & list and view function below the imports
+class Sushi:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, type, description, price):
+    self.name = name
+    self.type = type
+    self.description = description
+    self.price = price
+
+sushi_list = [
+  Sushi('California', 'sushi roll', 'cucumber, avocado, and crab', 8),
+  Sushi('Tuna', 'sashimi', 'delicious', 10),
+  Sushi('Cucumber', 'sushi roll', 'vegan friendly', 4),
+  Sushi('Salmon', 'nigiri', 'sustainably caught salmon', 8),
+  Sushi('Captain Crunch Crab', 'sushi roll', 'epic, delicious treat, softshell crab battered in captain crunch batter and fried', 10)
+]
+
 #define the home view
 def home(request):
   return HttpResponse('<h1>Hello Sushi Fans!</h1>')
@@ -9,3 +25,5 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+def sushi_index(request):
+  return render(request, 'sushi/index.html', { 'sushi_list': sushi_list })
