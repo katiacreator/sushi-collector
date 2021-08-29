4e7ffbb921568faf2021a7d8f64200ed5d5abe75
katiacreator <katia.creator@gmail.com>
2021-08-29 16:44:27 -0400

    create MVT for sushi index, migrated models to database

--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Sushi
 
 # Add the Sushi class & list and view function below the imports
-class Sushi:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, type, description, price):
+# class Sushi:  # Note that parens are optional if not inheriting from another class
+"""   def __init__(self, name, type, description, price):
     self.name = name
     self.type = type
     self.description = description
-    self.price = price
+    self.price = price """
 
-sushi_list = [
+""" rolls = [
   Sushi('California', 'sushi roll', 'cucumber, avocado, and crab', 8),
   Sushi('Tuna', 'sashimi', 'delicious', 10),
   Sushi('Cucumber', 'sushi roll', 'vegan friendly', 4),
   Sushi('Salmon', 'nigiri', 'sustainably caught salmon', 8),
   Sushi('Captain Crunch Crab', 'sushi roll', 'epic, delicious treat, softshell crab battered in captain crunch batter and fried', 10)
-]
+] """
 
 #define the home view
 def home(request):
-  return HttpResponse('<h1>Hello Sushi Fans!</h1>')
+  return render(request, 'home.html')
 
 #define the about view
 def about(request):
   return render(request, 'about.html')
 
 def sushi_index(request):
-  return render(request, 'sushi/index.html', { 'sushi_list': sushi_list })
+  rolls = Sushi.objects.all()
+  return render(request, 'sushi/index.html', { 'rolls': rolls})
