1e21826305e7750465a9539a52e7a8a35cbb9ccb
katiacreator <katia.creator@gmail.com>
2021-08-28 21:03:10 -0400

    added about.html template, created static directories, added logo icon image to base.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
 #define the home view
 def home(request):
@@ -8,5 +7,5 @@ def home(request):
 
 #define the about view
 def about(request):
-  return HttpResponse('<h1>About Sushi</h1>')
+  return render(request, 'about.html')
 
