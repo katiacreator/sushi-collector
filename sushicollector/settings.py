d163dbd25d3e7454afe31692874f2d085bd63afd
katiacreator <katia.creator@gmail.com>
2021-09-01 19:24:19 -0400

    deployment to heroku test

--- sushicollector/settings.py
+++ sushicollector/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-from os import environ
+import environ
 env = environ.Env()
 environ.Env.read_env()
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
